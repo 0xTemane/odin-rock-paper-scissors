b13e3793f21717609f661b4ce5f4096e317beba7
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-04-26 11:31:22 +0200

    Create getHumanChoice() & rename GetComputerChoice
    GetComputerChoice() -> getComputerChoice()

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("External Javascript works.");
 const choices = ["rock","paper","scissors"];
 
-function GetComputerChoice() {
+function getComputerChoice() {
     let x = Math.random() * 2;
     return choices[Math.round(x)];
+}
+
+function getHumanChoice() {
+    return prompt("Your turn! Rock, Paper or Scissors?  ").toLowerCase();
 }
