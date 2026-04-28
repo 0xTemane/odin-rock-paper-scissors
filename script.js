534da1d1ea691dba1368474221a64d58e97622fe
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-04-28 11:20:01 +0200

    Put a single console.log that shows the score
    The others ones have been deleted for code
    simplicity.

--- script.js
+++ script.js
@@ -16,40 +16,33 @@ function playGame(){
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice){
             console.log(`It's a tie: ${humanChoice} vs ${computerChoice}.`);
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         } else if (humanChoice === "rock" && computerChoice === "scissors"){
             console.log(`You won! ${humanChoice} vs ${computerChoice}.`);
             humanScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         } else if (humanChoice === "paper" && computerChoice === "rock"){
             console.log(`You won! ${humanChoice} vs ${computerChoice}.`);
             humanScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         } else if (humanChoice === "scissors" && computerChoice === "paper"){
             console.log(`You won! ${humanChoice} vs ${computerChoice}.`);
             humanScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         } else if (computerChoice === "rock" && humanChoice === "scissors"){
             console.log(`You lose! ${humanChoice} vs ${computerChoice}.`);
             computerScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         } else if (computerChoice === "paper" && humanChoice === "rock"){
             console.log(`You lose! ${humanChoice} vs ${computerChoice}.`);
             computerScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         }  else if (computerChoice === "scissors" && humanChoice === "paper"){
             console.log(`You lose! ${humanChoice} vs ${computerChoice}.`);
             computerScore++;
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         }  else {
             console.log("Hold up! Wait a minute, your choice ain't right...");
             console.log("SAME SCORE AS BEFORE")
-            console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
         }
     }
 
     for (let i=0; i < 5; i++) {
         playRound(getHumanChoice(),getComputerChoice());
+        console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
     }
 }
 
